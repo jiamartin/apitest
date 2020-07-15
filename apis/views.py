37338fe2c8c2323387e3fe7038fdafebe6c84883
jiamartin <julialagarde3@gmail.com>
2020-07-15 22:30:40 +0800

    07/15

--- apis/views.py
+++ apis/views.py
@@ -1,10 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import requests
 
 
 def index(request):
+        if request.method == 'POST':
+                url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=458f3827e9cbd3d04baa56e53dc349fd'
+                city = request.POST['city']
+                city_weather = requests.get(url.format(city)).json()
+          
+
+                weather = {
+                        'city' : city_weather['name'],
+                        'temperature' : city_weather['main']['temp'],
+                        'feels' : city_weather['main']['feels_like'],
+                        'humidity' : city_weather['main']['humidity'],
+                        'description' : city_weather['weather'][0]['description'],
+                        'icon' : city_weather['weather'][0]['icon'],
+                        'country' : city_weather['sys']['country']
+                        }
         
-        return render(request, 'index.html', {})
+                context = {'weather' : weather}
+                return render(request, 'index.html', context)
+
+               
+
+        else:
+                url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=458f3827e9cbd3d04baa56e53dc349fd'
+                city = 'Cabuyao'
+                city_weather = requests.get(url.format(city)).json()
+                       
+                weather = {
+                        'city' : city_weather['name'],
+                        'temperature' : city_weather['main']['temp'],
+                        'feels' : city_weather['main']['feels_like'],
+                        'humidity' : city_weather['main']['humidity'],
+                        'description' : city_weather['weather'][0]['description'],
+                        'icon' : city_weather['weather'][0]['icon'],
+                        'country' : city_weather['sys']['country']
+                        }
+
+                context = {'weather' : weather}
+ 
+                return render(request, 'index.html', context)
 
 
 def about(request):
