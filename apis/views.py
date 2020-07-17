85f7edfca11c1d8b03d7d28a5697ef0c7618120d
jiamartin <julialagarde3@gmail.com>
2020-07-17 17:10:28 +0800

    final

--- apis/views.py
+++ apis/views.py
@@ -6,23 +6,33 @@ def index(request):
         if request.method == 'POST':
                 url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=458f3827e9cbd3d04baa56e53dc349fd'
                 city = request.POST['city']
-                city_weather = requests.get(url.format(city)).json()
-          
 
-                weather = {
-                        'city' : city_weather['name'],
-                        'temperature' : city_weather['main']['temp'],
-                        'feels' : city_weather['main']['feels_like'],
-                        'humidity' : city_weather['main']['humidity'],
-                        'description' : city_weather['weather'][0]['description'],
-                        'icon' : city_weather['weather'][0]['icon'],
-                        'country' : city_weather['sys']['country']
-                        }
-        
-                context = {'weather' : weather}
-                return render(request, 'index.html', context)
+                try:
+                        city_weather = requests.get(url.format(city)).json()
+                
+                        weather = {
+                                'city' : city_weather['name'],
+                                'temperature' : city_weather['main']['temp'],
+                                'feels' : city_weather['main']['feels_like'],
+                                'humidity' : city_weather['main']['humidity'],
+                                'description' : city_weather['weather'][0]['description'],
+                                'icon' : city_weather['weather'][0]['icon'],
+                                'country' : city_weather['sys']['country']
+                                }
+                        
+                        context = {'weather' : weather}
+                        return render(request, 'index.html', context)
 
-               
+                except KeyError:
+                        pass
+                        city_weather = 'Error'
+                        return render(request, 'index.html', {'city_weather' : city_weather})
+
+                except EXCEPTION as e:
+                        pass
+                        city_weather = 'Error'
+                        return render(request, 'index.html', {'city_weather' : city_weather})
+
 
         else:
                 url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=458f3827e9cbd3d04baa56e53dc349fd'
